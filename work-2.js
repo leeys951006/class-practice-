3c3e36ff2c3d7de447f9d4ab337c8b6c3c7845a5
이연승 <leeys951006@gmail.com>
2024-06-04 17:17:18 +0900

    test

--- work-2.js
+++ work-2.js
@@ -1,1 +1,10 @@
-console.log();
+function workThree(index, name) {
+  let local = {
+    number: index + 1,
+    name: name,
+  };
+  return local;
+}
+
+let test = workThree(0, '이연승');
+console.log(test);
